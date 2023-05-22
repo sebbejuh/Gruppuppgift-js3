5a5cc3dd51c5ceab4e3558fc8196038e51c6b15e
Frawser <mikis011@hotmail.com>
2023-05-22 15:06:08 +0200

    Pages/navbar component

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaHotjar } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container d-flex">
+        <Link to="/" className="brand">
+          <FaHotjar></FaHotjar>
+        </Link>
+        <ul className="nav-links d-flex">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Products</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart">
+              <FaShoppingCart />
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="form-page">
+      <h1>Register New User</h1>
+      <form>
+        <div className="form-group">
+            <label htmlFor="firstName">First name:</label>
+            <input type="text" name="lastName" id="firstName" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="lastName">Last name:</label>
+            <input type="text" name="lastName" id="lastName" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="password" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input type="password" name="confirmPassword" id="confirmPassword" />
+        </div>
+        <button className="btn btn-primary">Register</button>
+
+      </form>
+    </div>
+  );
+};
+
+export default Register;
