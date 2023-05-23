fd1ac554faa2aa081267cc2288a5c9744f61390d
Sebastian Ivanoff <sebbe.ivanoff@gmail.com>
2023-05-23 21:33:11 +0200

    fetched user login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setLoginData((prevData) => {
+      return {
+        ...prevData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:7777/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div className="login-form-box">
       <div className="login-title">
         <p>Please Login to Your Account</p>
         <div className="login-title-border" />
       </div>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <div className="login-input">
           <div className="register-link">
             <label htmlFor="username">Username*</label>
@@ -15,7 +43,13 @@ const Login = () => {
               <span>Don't have an Account yet?</span>
             </NavLink>
           </div>
-          <input type="text" name="username" id="username" />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={loginData.username}
+            onChange={handleChange}
+          />
         </div>
         <div className="login-input">
           <div className="forgot-pw">
@@ -27,7 +61,13 @@ const Login = () => {
           <input type="password" name="password" id="password" />
         </div>
         <div className="login-checkbox">
-          <input type="checkbox" name="checkbox" id="remember" />
+          <input
+            type="checkbox"
+            name="checkbox"
+            id="remember"
+            value={loginData.password}
+            onChange={handleChange}
+          />
           <label htmlFor="checkbox">Please keep me logged in</label>
         </div>
         <button className="login-btn">Submit</button>
