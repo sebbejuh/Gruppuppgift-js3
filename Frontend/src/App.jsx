b10daddeaa331b6aa2eeca5ec7b066acaf859903
Peter Hogler <me@peterhogler.dev>
2023-05-23 12:10:12 +0200

    added contact page markup and styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 //components
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 
 //sass files
-import "./index.scss"
-import "./App.scss"
-
+import "./index.scss";
+import "./App.scss";
 
 //pages
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Products from './pages/Products'
-import Cart from './pages/Cart'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return (
-    <>
-    <Navbar/>
-    <div className='container'>
-        <Routes>
-            <Route index element={<Home/>} />
-            <Route path='login' element={<Login/>} />
-            <Route path='register' element={<Register/>} />
-            <Route path='products' element={<Products/>} />
-            <Route path='cart' element={<Cart/>} />
-        </Routes>
-    </div>
-    </>
-  )
-}
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="products" element={<Products />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="cart" element={<Cart />} />
+                </Routes>
+            </div>
+        </>
+    );
+};
 
-export default App
+export default App;
